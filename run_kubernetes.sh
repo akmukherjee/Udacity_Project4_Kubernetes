5be2b9e2fb1a845ae129fe9ad93e20e40f0f05ff
EC2 Default User <ec2-user@ip-172-31-21-22.us-west-2.compute.internal>
2019-09-13 21:14:48 +0000

    Adding the Kubernetes Task contents

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="akmukherjee/udacity-docker-predictor"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-docker-predictor --image=$dockerpath --port=80
 
-
+#Waiting for Pods to be ready
+sleep 20
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/udacity-docker-predictor 8000:80
 
